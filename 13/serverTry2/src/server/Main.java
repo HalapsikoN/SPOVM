22f340b20096c5deb250697ed5f877e2072b0fad
Dima Alhovik <halapsikon@mail.ru>
2019-04-18 01:26:29 +0300

    help me

--- 13/serverTry2/src/server/Main.java
+++ 13/serverTry2/src/server/Main.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 @WebServlet("/server")
 public class Main extends HttpServlet {
 
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getRequestDispatcher("/WEB-INF/mainPage.jsp").forward(req,resp);
